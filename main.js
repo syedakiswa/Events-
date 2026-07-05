967672ee129272596f51948a2301c1575adcf995
syeda kiswa <syedakiswa710@gmail.com>
2026-07-05 13:07:21 +0500

    inital

--- main.js
+++ main.js
@@ -8,13 +8,13 @@
 
 
 
-btn.addEventListener('keydown', (e) =>{ // e convention 
-    console.log(e.target)
-})
+// btn.addEventListener('keydown', (e) =>{ // e convention 
+//     console.log(e.target)
+// })
 
-window.addEventListener('resize', (e)=>{
+// window.addEventListener('resize', (e)=>{
 
-})
+// })
 
 
 // e.target - woh elements target kro jis par exactly event hua
@@ -29,3 +29,36 @@ window.addEventListener('resize', (e)=>{
 // e.clientx / y - mouse ki position screen par
 // e.client = 340 
 // pixels from left
+
+
+let btn = document.getElementById('buybtn');
+let status = document.getElementById('status');
+
+
+// click 
+btn.addEventListener('click', (e) =>{
+   status.innerText = 'Product addet to cart'
+
+})
+
+// Double Click 
+btn.addEventListener('dblclick', (e) =>{
+    status.innerText = 'Addet to wishlist'
+})
+
+
+// mouseover
+btn.addEventListener('mouseover', (e) =>{  
+    btn.style.backgroundColor = "green"
+    btn.style.color = 'white'
+    status.innerText = 'Ready to Buy'
+})
+
+
+// mouseout
+btn.addEventListener('mouseout', (e) =>{
+    btn.style.backgroundColor = '';
+    btn.style.color = ''
+    status.
+
+})
