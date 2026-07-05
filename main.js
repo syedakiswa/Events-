03c4390511a84eb43fcf9331aeff6df3f0276be3
syeda kiswa <syedakiswa710@gmail.com>
2026-07-05 12:19:57 +0500

    Inital commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// let btn = document.getElementById("btn")
+// let body = document.getElementById("body")
+
+// btn.addEventListener('click', () => {
+//     body.style.backgroundColor = "black";
+//     body.style.color = 'white'
+// })
+
+
+
+btn.addEventListener('keydown', (e) =>{ // e convention 
+    console.log(e.target)
+})
+
+window.addEventListener('resize', (e)=>{
+
+})
+
+
+// e.target - woh elements target kro jis par exactly event hua
+// e.target.textContent
+
